1db73bb24bad9aff329be95b08261b5269075560
Marcio Vinicius <marciovmartins@hotmail.com>
2021-11-23 10:47:21 -0300

    HelloWorldController to use HelloWorldService

--- asm/src/main/kotlin/com/github/marciovmartins/futsitev3/AsmApplication.kt
+++ asm/src/main/kotlin/com/github/marciovmartins/futsitev3/AsmApplication.kt
@@ -4,6 +4,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Controller
+import org.springframework.stereotype.Service
 import org.springframework.web.bind.annotation.GetMapping
 
 @SpringBootApplication
@@ -14,11 +15,15 @@ fun main(args: Array<String>) {
 }
 
 @Controller
-class HelloWorldController {
+class HelloWorldController(
+    private val helloWorldService: HelloWorldService
+) {
+
     @GetMapping("/")
-    fun helloWorld(): ResponseEntity<String> = ResponseEntity.ok("Hello World")
+    fun helloWorld(): ResponseEntity<String> = ResponseEntity.ok(helloWorldService.call())
 }
 
+@Service
 class HelloWorldService {
     fun call() = "Hello World!"
 }
