ca1098da77864e48ca339910f878add82e14540b
Marcio Vinicius <marciovmartins@hotmail.com>
2022-03-20 10:10:14 +0100

    Do not allow get all user data game days

--- user-data/src/main/kotlin/com/github/marciovmartins/futsitev3/user/data/gameDay/GameDayRepository.kt
+++ user-data/src/main/kotlin/com/github/marciovmartins/futsitev3/user/data/gameDay/GameDayRepository.kt
@@ -2,7 +2,11 @@ package com.github.marciovmartins.futsitev3.user.data.gameDay
 
 import org.springframework.data.repository.CrudRepository
 import org.springframework.data.rest.core.annotation.RepositoryRestResource
+import org.springframework.data.rest.core.annotation.RestResource
 import java.util.UUID
 
 @RepositoryRestResource
-interface GameDayController : CrudRepository<GameDay, UUID>
+interface GameDayRepository : CrudRepository<GameDay, UUID> {
+    @RestResource(exported = false)
+    override fun findAll(): MutableIterable<GameDay>
+}
--- user-data/src/test/kotlin/com/github/marciovmartins/futsitev3/user/data/gameDay/GameDayRepositoryIT.kt
+++ user-data/src/test/kotlin/com/github/marciovmartins/futsitev3/user/data/gameDay/GameDayRepositoryIT.kt
@@ -3,11 +3,13 @@ package com.github.marciovmartins.futsitev3.user.data.gameDay
 import com.github.marciovmartins.futsitev3.user.data.BaseIT
 import com.github.marciovmartins.futsitev3.user.data.gameDay.argumentsprovider.ValidGameDayArgumentsProvider
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.ArgumentsSource
+import org.springframework.http.HttpStatus
 import java.util.UUID
 
-class GameDayControllerIT : BaseIT() {
+class GameDayRepositoryIT : BaseIT() {
     @ParameterizedTest(name = "{0}")
     @ArgumentsSource(ValidGameDayArgumentsProvider::class)
     fun `create and retrieve a game day`(
@@ -35,5 +37,16 @@ class GameDayControllerIT : BaseIT() {
             .isNotNull
             .usingRecursiveComparison()
             .isEqualTo(gameDayToCreate)
+    }
+
+    @Test
+    fun `do not allow get all game days`() {
+        // execution
+        val response = webTestClient.get()
+            .uri("gameDays")
+            .exchange()
+
+        // assertions
+        response.expectStatus().isEqualTo(HttpStatus.METHOD_NOT_ALLOWED)
     }
 }
