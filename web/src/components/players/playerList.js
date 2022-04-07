19222239b715913b3a402f4e9bf72907d6f3a0b4
Marcio Vinicius <marciovmartins@hotmail.com>
2022-04-07 23:38:49 +0200

    expanded player list to remove a player

--- web/src/components/players/playerList.js
+++ web/src/components/players/playerList.js
@@ -5,20 +5,9 @@ import {extractId} from "../helper";
 const userDataPlayersHref = 'http://localhost:8081/players';
 
 export function PlayerList() {
-    const amateurSoccerGroupId = sessionStorage.getItem('amateurSoccerGroupId');
     const [state, setState] = useState({players: []});
 
-    const loadList = () => {
-        fetch(userDataPlayersHref + '/search/byAmateurSoccerGroupId?amateurSoccerGroupId=' + amateurSoccerGroupId, {
-            method: 'GET',
-            headers: {"Accept": "application/hal+json"},
-            mode: "cors"
-        }).then(response => response.json())
-            .then(data => data._embedded.players)
-            .then(players => setState({players}))
-    };
-
-    useEffect(loadList, []);
+    useEffect(() => loadList(setState), []);
 
     return <main>
         <h2>Players</h2>
@@ -26,8 +15,29 @@ export function PlayerList() {
             {state.players.map((player, index) =>
                 <li key={index}>
                     <Link to={'/players/' + extractId(player._links.self.href)}>{player.nickname}</Link>
+                    &nbsp;
+                    <a href='#' onClick={(e) => removePlayer(e, player._links.self.href, setState)}>[remove]</a>
                 </li>
             )}
         </ul>
     </main>;
+}
+
+function loadList(setState) {
+    const amateurSoccerGroupId = sessionStorage.getItem('amateurSoccerGroupId');
+    fetch(userDataPlayersHref + '/search/byAmateurSoccerGroupId?amateurSoccerGroupId=' + amateurSoccerGroupId, {
+        method: 'GET',
+        headers: {"Accept": "application/hal+json"},
+        mode: "cors"
+    }).then(response => response.json())
+        .then(data => data._embedded.players)
+        .then(players => setState({players}))
+};
+
+async function removePlayer(event, playerUri, setState) {
+    await fetch(playerUri, {
+        method: 'DELETE',
+        headers: {'Content-type': 'application/hal+json'},
+    })
+    loadList(setState);
 }
