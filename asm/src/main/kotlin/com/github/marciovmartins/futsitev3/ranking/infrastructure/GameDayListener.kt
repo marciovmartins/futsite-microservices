2a888e4622b4ec6b9a8281d4e7376b002d0b1b67
Marcio Vinicius <marciovmartins@hotmail.com>
2022-05-27 23:40:41 +0200

    Refactored GameDayListener using RabbitListener bindings configuration instead of two beans configurations (2)

--- asm/src/main/kotlin/com/github/marciovmartins/futsitev3/ranking/infrastructure/GameDayListener.kt
+++ asm/src/main/kotlin/com/github/marciovmartins/futsitev3/ranking/infrastructure/GameDayListener.kt
@@ -2,16 +2,27 @@ package com.github.marciovmartins.futsitev3.ranking.infrastructure
 
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.github.marciovmartins.futsitev3.ranking.usecase.GetPlayerStatistic
+import org.springframework.amqp.rabbit.annotation.Exchange
+import org.springframework.amqp.rabbit.annotation.Queue
+import org.springframework.amqp.rabbit.annotation.QueueBinding
 import org.springframework.amqp.rabbit.annotation.RabbitListener
 import org.springframework.stereotype.Component
 import java.util.UUID
+
+private const val gameDayCreatedRoutingKey = "futsitev3.gameday.created"
 
 @Component
 class GameDayListener(
     private val getPlayerStatistic: GetPlayerStatistic,
     private val objectMapper: ObjectMapper,
 ) {
-    @RabbitListener(queues = ["futsitev3.gameday.created"])
+    @RabbitListener(
+        bindings = [QueueBinding(
+            value = Queue(gameDayCreatedRoutingKey),
+            exchange = Exchange("amq.topic", type = "topic"),
+            key = [gameDayCreatedRoutingKey]
+        )]
+    )
     fun receiveGameDayCreatedMessage(messageIn: String) {
         try {
             val gameDay = objectMapper.readValue(messageIn, GameDayCreatedDTO::class.java)
